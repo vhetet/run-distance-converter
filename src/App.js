fb167466d4c36827176fcd058c9d1a8274c52912
Vincent <vincent.hetet@gmail.com>
2019-07-28 09:34:09 -0500

    replace css with scss and add units in the form result

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../node_modules/purecss/build/pure-min.css';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   constructor(props) {
@@ -28,41 +28,48 @@ class App extends Component {
         <form onSubmit={this.handleSubmit} className="pure-form">
           <label>
             <span>distance in miles:</span>
-          <input type="text" value={this.state.distance} onChange={this.handleDistanceChange} />
+            <input type="text" value={this.state.distance} onChange={this.handleDistanceChange} />
           </label>
           <label>
             <span>time in minutes:</span>
-          <input type="text" value={this.state.time} onChange={this.handleTimeChange} />
+            <input type="text" value={this.state.time} onChange={this.handleTimeChange} />
           </label>
         </form>
         <div className="result pure-form">
           <p>
-            <span>result in km: </span>
-            <span>{this.state.result}</span>
+            <span className="left">result in km: </span>
+            <span className="right">{this.state.result}</span>
+            <span className="unit">km</span>
           </p>
           <p>
-            <span>pace in min/km: </span>
-            <span>{this.state.time && (this.state.time / (this.state.distance * 1.60934)).toFixed(3)}</span>
+            <span className="left">pace in min/km: </span>
+            <span className="right">{this.state.time && (this.state.time / (this.state.distance * 1.60934)).toFixed(3)}</span>
+            <span className="unit">min/km</span>
           </p>
           <p>
-            <span>pace in min/miles: </span>
-            <span>{this.state.time && (this.state.time / this.state.distance).toFixed(3)}</span>
+            <span className="left">pace in min/miles: </span>
+            <span className="right">{this.state.time && (this.state.time / this.state.distance).toFixed(3)}</span>
+            <span className="unit">min/miles</span>
           </p>
           <p>
-            <span>equivalence for 5km: </span>
-            <span>{this.state.time && ((this.state.time / (this.state.distance * 1.60934)) * 5).toFixed(3)}</span>
+            <span className="left">equivalence for 5km: </span>
+            <span className="right">{this.state.time && ((this.state.time / (this.state.distance * 1.60934)) * 5).toFixed(3)}</span>
+            <span className="unit">5km</span>
           </p>
           <p>
-            <span>equivalence for 10km: </span>
-            <span>{this.state.time && ((this.state.time / (this.state.distance * 1.60934)) * 10).toFixed(3)}</span>
+            <span className="left">equivalence for 10km: </span>
+            <span className="right">{this.state.time && ((this.state.time / (this.state.distance * 1.60934)) * 10).toFixed(3)}</span>
+            <span className="unit">10km</span>
           </p>
           <p>
-            <span>equivalence for half-marathon: </span>
-            <span>{this.state.time && ((this.state.time / (this.state.distance * 1.60934)) * 21.1).toFixed(3)}</span>
+            <span className="left">equivalence for half-marathon: </span>
+            <span className="right">{this.state.time && ((this.state.time / (this.state.distance * 1.60934)) * 21.1).toFixed(3)}</span>
+            <span className="unit">half-marathon</span>
           </p>
           <p>
-            <span>equivalence for 3.5 miles: </span>
-            <span>{this.state.time && ((this.state.time / this.state.distance) * 3.5).toFixed(3)}</span>
+            <span className="left">equivalence for 3.5 miles: </span>
+            <span className="right">{this.state.time && ((this.state.time / this.state.distance) * 3.5).toFixed(3)}</span>
+            <span className="unit">3.5 miles</span>
           </p>
         </div>
       </div>
